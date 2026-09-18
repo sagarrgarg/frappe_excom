c2a6cdbf53dd875d0686f06f0f2cd37ddb04e816
somilvaishya <somilsearchosis@gmail.com>
2026-09-18 13:38:52 +0000

    build(frontend): rebuild with the Activity report

--- excom/public/excom/assets/AdminPage-CEoIC707.js
+++ excom/public/excom/assets/AdminPage-CEoIC707.js
@@ -1,4 +1,4 @@
-import{c as h,j as e,Z as o,B as p,a as i}from"./index-BJAtvdqc.js";import{au as u}from"./index-D1IJWwOd.js";/**
+import{c as h,j as e,Z as o,B as p,a as i}from"./index-CgaffhYA.js";import{au as u}from"./index-CclV6EgG.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
